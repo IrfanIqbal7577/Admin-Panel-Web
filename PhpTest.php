7dd222c816e10723c97bb2212654ac892ba34ed8
irfaniqbal7577 <irfaniqbal7577@gmail.com>
2022-11-11 22:36:31 +0500

    Admin Panel for University

---
+++ PhpTest.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+$c=$x+$y;
+echo "<br>";
+echo $c;
+echo "<br>";
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+</body>
+</html>
